2964c7c19f0a24c16422e5328f9d12d8131ffd47
Parsa <parsasaiednia@gmail.com>
2026-01-01 17:01:13 +0330

    feat: implement MinResource scheduler

--- Computer-Aided-Designs-CA5/src/scheduler.py
+++ Computer-Aided-Designs-CA5/src/scheduler.py
@@ -102,47 +102,95 @@ class MinLatencyScheduler(ListScheduler):
     def __init__(self, dfg_root : BaseNode, numof_resources : dict):
         super().__init__(dfg_root=dfg_root, numof_reources=numof_resources)
 
-    # TODO
+    # TODO: Implemented in a separate branch
     def find_candidate_nodes(self) -> List[OperatorNode]:
         pass
 
-    # TODO
+    # TODO: Implemented in a separate branch
     def select_from_frontier(self, frontier : dict) -> List[OperatorNode]:
         pass
 
-    # TODO
+    # TODO: Implemented in a separate branch
     def schedule(self) -> None:
         pass
 
-
-    
-
-    
 class MinResourceScheduler(ListScheduler):
     def __init__(self, dfg_root : BaseNode, numof_resources : dict, max_time : int):
         super().__init__(dfg_root=dfg_root, numof_reources=numof_resources)
         self.max_time = max_time
-        self.latest_time = self.find_latest_times()
-
+        self.latest_times = self.find_latest_times()
+        self.current_time = 0 
 
     '''
-        Assigns the latest possible time for each node to be executed.
-        It's used on Minimum-Resource, Latency-Constrained algorithm.
+        Assigns the latest possible time for each node to be executed (ALAP).
     '''
-    # TODO
     def find_latest_times(self) -> dict:
-        latest_time = dict()
-        # ...
-        return latest_time
-   
-    # TODO
+        latest = {node.id: float('inf') for node in self.all_operators}
+        
+        sorted_ops = sorted(self.all_operators, key=lambda x: x.depth)
+        
+        latest[self.root.id] = self.max_time
+        
+        for node in sorted_ops:
+            parent_deadline = latest[node.id]
+            
+            for operand in node.operands:
+                if isinstance(operand, OperatorNode):
+                    current_child_deadline = latest.get(operand.id, float('inf'))
+                    latest[operand.id] = min(current_child_deadline, parent_deadline - 1)
+                    
+        return latest
+
     def find_candidate_nodes(self) -> List[OperatorNode]:
-        pass
+        candidates = []
+        for node in self.all_operators:
+            if node.id in self.scheduled_ids:
+                continue
+            
+            if self.is_node_ready(node):
+                candidates.append(node)
+        return candidates
 
-    # TODO
     def select_from_frontier(self, frontier : dict) -> List[OperatorNode]:
-        pass
+        selected_nodes = []
+        
+        for res_type, nodes in frontier.items():
+            limit = self.numof_resources.get(res_type, 0)
+            
+            node_slacks = []
+            for node in nodes:
+                slack = self.latest_times.get(node.id, self.max_time) - self.current_time
+                node_slacks.append((slack, node))
+            
+            node_slacks.sort(key=lambda x: (x[0], x[1].id))
+            
+            top_nodes = [x[1] for x in node_slacks[:limit]]
+            selected_nodes.extend(top_nodes)
+            
+        return selected_nodes
 
-    # TODO
     def schedule(self) -> None:
-        pass
+        self.current_time = 1
+        
+        while len(self.scheduled_ids) < len(self.all_operators):
+            candidates = self.find_candidate_nodes()
+            
+            frontier = {op: [] for op in OP_TYPES}
+            for node in candidates:
+                if node.op_type in frontier:
+                    frontier[node.op_type].append(node)
+            
+            selected = self.select_from_frontier(frontier)
+            
+            resource_counters = {op: 1 for op in OP_TYPES}
+            for node in selected:
+                res_id = resource_counters[node.op_type]
+                resource_counters[node.op_type] += 1
+                
+                self.record_scheduled_node(node, self.current_time, res_id)
+                
+            self.current_time += 1
+            
+            if self.current_time > self.max_time + 10: 
+                print("Warning: Schedule exceeded max_time significantly. Possible deadlock or impossible constraint.")
+                break
